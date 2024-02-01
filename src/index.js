4f80e9e036942429435b110b3394e470eaa83cf0
Valerian Marta <demer.marta@gmail.com>
2024-02-01 14:23:20 +0200

    Added embed announcements!

--- src/index.js
+++ src/index.js
@@ -5,27 +5,37 @@ const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits
 client.on(Events.ClientReady, (x) => {
     client.user.setActivity("I'm soulless");
 
-    const announe = new SlashCommandBuilder()
-	.setName('announce')
-	.setDescription('Announce something!')
-    .addStringOption(option =>
-        option
-        .setName('text')
-        .setDescription('The text to announce')
-        .setRequired(true)
-        )
-    .addUserOption(option => 
-        option
-        .setName('user')
-        .setDescription('Person to mention')
-        .setRequired(false)
-        )
-    
-    client.application.commands.create(announe);
-
     const embed = new SlashCommandBuilder()
     .setName('embed')
-    .setDescription('Create embeded message');
+    .setDescription('Create embeded message')
+    .addBooleanOption(option =>
+        option
+        .setName('use_everyone')
+        .setDescription('Do you want to mention everyone?')
+        .setRequired(true) 
+    )
+    .addStringOption(option =>
+        option
+        .setName('embeded_message')
+        .setDescription('Set the message of announce')
+        .setRequired(true),
+    )
+    .addChannelOption(option =>
+        option
+        .setName('set_channel')
+        .setDescription('Specify the channel where you want to use message')
+        .setRequired(false)
+    );
+
+    for (let i = 1; i <= 6; i++) {
+        const fieldName = `additional_field${i}`;
+        embed.addStringOption(option =>
+            option
+            .setName(`${fieldName}`)
+            .setDescription('Additional Field in the announce (new line)')    
+            .setRequired(false)
+        )
+    }
 
     client.application.commands.create(embed);
 
@@ -34,6 +44,10 @@ client.on(Events.ClientReady, (x) => {
 })
 
 client.on("interactionCreate", (interaction) =>{
+    const currentUser = interaction.user.tag;
+    const currentUserImage = interaction.user.displayAvatarURL({ format: "png", dynamic: true });
+    const channelAvatar = interaction.guild ? interaction.guild.iconURL({ format: "png", dynamic: true }) : null;
+
     if(!interaction.isChatInputCommand()) return;
 
     if(interaction.commandName === 'announce'){
@@ -49,36 +63,58 @@ client.on("interactionCreate", (interaction) =>{
     }
 
     if(interaction.commandName === 'embed'){
+        const userDesc = interaction.options.getString('embeded_message');
+        const mentionChannel = interaction.options.getChannel('set_channel');
+        const useEveryone = interaction.options.getBoolean('use_everyone');
+        const mentionEveryoneString = useEveryone ? '@everyone ' : '';
+
+        const fields = [];
+
+        for (let i = 1; i <= 6; i++) {
+            const fieldName = `additional_field${i}`;
+            const fieldValue = interaction.options.getString(fieldName);
+            
+            if (fieldValue) {
+                fields.push({
+                    name: `News Nr.${i}`,
+                    value: fieldValue,
+                    inline: false,
+                });
+            }
+        }
+
         const embed = new EmbedBuilder()
         .setTitle('News!')
-        .setDescription('Embed message')
+        .setDescription(`${mentionEveryoneString} ${userDesc.toString()}.`)
         .setColor(0x206694)
-        .setFooter({text: 'Footer text'})
+        .setThumbnail(channelAvatar)
+        .setFooter({text: currentUser, iconURL: currentUserImage})
         .setTimestamp()
-        .setImage('https://i.ibb.co/6s6S4GG/important.png')
-        .addFields({
-            name: 'Field1',
-            value: 'Test text',
-            inline: true
-        },{
-            name: 'Field2',
-            value: 'Text fields 2',
-            inline: true
-        },{
-            name: 'Field3',
-            value: 'Text fields 3',
-            inline: true
-        },{
-            name: 'Field4',
-            value: 'Text fields 4',
-            inline: true
-        },{
-            name: 'Field5',
-            value: 'Text fields 5',
-            inline: true
-        })
-        interaction.reply({embeds: [embed]});
+        .setImage('https://i.ibb.co/6s6S4GG/important.png');
 
+        if (fields.length > 0) {
+            embed.addFields(...fields);
+        }
+
+        if (mentionChannel) {
+            mentionChannel.send({ embeds: [embed] })
+                .then(() => {
+                    // Respond to the interaction acknowledging that the message was sent
+                    interaction.reply('Message sent successfully!');
+                })
+                .catch(error => {
+                    console.error('Error sending message:', error);
+                    // Respond to the interaction with an error message
+                    interaction.reply('There was an error sending the message.');
+                });
+        } else {
+            interaction.reply({ embeds: [embed] })
+                .catch(error => {
+                    console.error('Error replying with message:', error);
+                    // Handle the error appropriately, such as logging it
+                });
+        }
+        
     }
 })
 
