c3eb1b721913e4f956e50a38aa6c7730716b6312
demer <marta.valerian@mail.ru>
2024-02-04 13:53:32 +0200

    Added leveling system

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ const {
     Permissions, 
     SlashCommandBuilder
 } = require('discord.js');
+
+const mongoose = require('mongoose');
+
 const eventHandler = require('./handlers/eventHandler');
 
 const client = new Client({
@@ -18,6 +21,17 @@ const client = new Client({
     ],
 });
 
-eventHandler(client);
+(async () => {
+    try {
+        mongoose.set('strictQuery', false);
+        await mongoose.connect(process.env.DB_CONNECTION);
+        console.log("Connected to DB.");
 
-client.login(process.env.TOKEN);
+
+        eventHandler(client);
+
+        client.login(process.env.TOKEN);
+    } catch (error) {
+        console.log(`Error when trying to connect to DB: ${error}`);
+    }
+})();
