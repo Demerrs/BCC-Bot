4d12ca5eb04920e19319c23a3c2da202c2a4eaf9
Valerian Marta <demer.marta@gmail.com>
2024-02-07 17:55:25 +0200

    Fixed error about await replies

--- src/events/interactionCreate/handleReports.js
+++ src/events/interactionCreate/handleReports.js
@@ -16,48 +16,46 @@ const {
  * @returns 
  */
 module.exports = async (client, interaction) => {
-    if(!interaction.isButton() || !interaction.customId) return;
+    if (!interaction.isButton() || !interaction.customId) return;
 
     try {
         const [type] = interaction.customId.split('.');
 
-        if(!type) return;
+        if (!type) return;
 
-        if(type !== 'takeReport' && type !== 'abortReport' && type !== 'finishReport') return;
+        if (type !== 'takeReport' && type !== 'abortReport' && type !== 'finishReport') return;
 
-        if(type == 'takeReport'){
-
-            const confirm = new ButtonBuilder()
-            .setCustomId('finishReport')
-            .setLabel('Finish report')
-            .setStyle(ButtonStyle.Success);
+        if (type == 'takeReport') {
+            const finish = new ButtonBuilder()
+                .setCustomId('finishReport')
+                .setLabel('Finish report')
+                .setStyle(ButtonStyle.Success);
 
             const cancel = new ButtonBuilder()
-            .setCustomId('abortReport')
-            .setLabel('Abort report')
-            .setStyle(ButtonStyle.Danger);
+                .setCustomId('abortReport')
+                .setLabel('Abort report')
+                .setStyle(ButtonStyle.Danger);
 
             const row = new ActionRowBuilder()
-            .addComponents(confirm, cancel);
-            
-            await interaction.message.edit({ content: `Taken by: ${interaction.user.displayName}\n\n`, components: [row]});
+                .addComponents(finish, cancel);
+
+            await interaction.message.edit({ content: `Taken by: ${interaction.user.displayName}\n\n`, components: [row] });
 
             const userToMessage = await client.users.fetch(interaction.message.embeds[0].fields[1].value);
 
-            //userToMessage.send(`**Your report is taken by: ${interaction.user.displayName}.**\nIf additional information is needed, ${interaction.user.displayName} will write to you.`);
+            await userToMessage.send(`**Your report is taken by: ${interaction.user.displayName}.**\nIf additional information is needed, ${interaction.user.displayName} will write to you.`);
 
             await interaction.reply({ content: `You took the report.`, ephemeral: true });
             return;
         }
 
-        if(type == 'abortReport'){
-
+        if (type == 'abortReport') {
             const modal = new ModalBuilder({
                 customId: `reportModalDecline-${interaction.user.id}`,
                 title: 'Decline Report',
-    
+
             });
-    
+
             const reportContent = new TextInputBuilder({
                 customId: 'reportContentDecline',
                 label: `Enter your reason`,
@@ -70,35 +68,29 @@ module.exports = async (client, interaction) => {
 
             await interaction.showModal(modal);
 
-            const filter = (interaction) => interaction.customId ===  `reportModalDecline-${interaction.user.id}`;
+            const filter = (interaction) => interaction.customId === `reportModalDecline-${interaction.user.id}`;
 
-            interaction.awaitModalSubmit({ filter, time: 120_000 })
-            .then(async (modalInteraction) => {
-                const reportContentValue = modalInteraction.fields.getTextInputValue('reportContentDecline');
+            const modalInteraction = await interaction.awaitModalSubmit({ filter, time: 120_000 });
 
-                //Other operations
-                await interaction.message.edit({content: `Declined by: ${interaction.user.displayName}\nReason: ${reportContentValue}`, components: []});
+            const reportContentValue = modalInteraction.fields.getTextInputValue('reportContentDecline');
 
-                const userToMessage = await client.users.fetch(interaction.message.embeds[0].fields[1].value);
+            await interaction.message.edit({ content: `Report was declined by: ${interaction.user.displayName}\nReason: ${reportContentValue}`, components: [] });
 
-                userToMessage.send(`**Your report "${interaction.message.embeds[0].title}" was declined by: ${interaction.user.displayName}.**\nReason: ${reportContentValue}`);
+            const userToMessage = await client.users.fetch(interaction.message.embeds[0].fields[1].value);
 
-                await modalInteraction.reply({content: `Report declined successfully.`, ephemeral: true});
+            await userToMessage.send(`**Your report "${interaction.message.embeds[0].title}" was declined.**\nReason: ${reportContentValue}`);
 
-                return;
-            })
-            .catch((e) => {
-                console.log(`Failed to submit modal: ${e}`);
-            });
+            await modalInteraction.reply({ content: `Report declined successfully.`, ephemeral: true });
+
+            return;
         }
-        
-        if(type == 'finishReport'){
+
+        if (type == 'finishReport') {
             const modal = new ModalBuilder({
                 customId: `reportModalFinish-${interaction.user.id}`,
                 title: 'Finish Report',
-    
             });
-    
+
             const reportContent = new TextInputBuilder({
                 customId: 'reportContentFinish',
                 label: `Enter your conclusion`,
@@ -111,31 +103,26 @@ module.exports = async (client, interaction) => {
 
             await interaction.showModal(modal);
 
-            const filter = (interaction) => interaction.customId ===  `reportModalFinish-${interaction.user.id}`;
+            const filter = (interaction) => interaction.customId === `reportModalFinish-${interaction.user.id}`;
 
-            interaction.awaitModalSubmit({ filter, time: 120_000 })
-            .then(async (modalInteraction) => {
-                const reportContentValue = modalInteraction.fields.getTextInputValue('reportContentFinish');
+            const modalInteraction = await interaction.awaitModalSubmit({ filter, time: 120_000 });
 
-                //Other operations
-                await interaction.message.edit({content: `Report was done by: ${interaction.user.displayName}\nConclusion: ${reportContentValue}`, components: []});
+            const reportContentValue = modalInteraction.fields.getTextInputValue('reportContentFinish');
 
-                const userToMessage = await client.users.fetch(interaction.message.embeds[0].fields[1].value);
+            await interaction.message.edit({ content: `Report was done by: ${interaction.user.displayName}\nConclusion: ${reportContentValue}`, components: [] });
 
-                userToMessage.send(`**Your report "${interaction.message.embeds[0].title}" was done.**\nConclusion: ${reportContentValue}`);
+            const userToMessage = await client.users.fetch(interaction.message.embeds[0].fields[1].value);
 
-                await modalInteraction.reply({content: `Report finished successfully.`, ephemeral: true});
+            await userToMessage.send(`**Your report "${interaction.message.embeds[0].title}" was done.**\nConclusion: ${reportContentValue}`);
 
-                return;
-            })
-            .catch((e) => {
-                console.log(`Failed to submit modal: ${e}`);
-            });
-        }else{
+            await modalInteraction.reply({ content: `Report finished successfully.`, ephemeral: true });
+
+            return;
+        } else {
             await interaction.reply({ content: `Error.`, ephemeral: true });
             return;
         }
-        
+
     } catch (error) {
         console.log(`The error appeared in buttons handling: ${error}`);
     }
